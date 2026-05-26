b1de5b05aff5ce03a194f813cc89df90e31bab5e
bishesh-ganesh-shrestha <unikbisheshshrestha12@gmail.com>
2026-05-27 02:27:40 +0545

    add authentication for avo admin

--- config/initializers/avo.rb
+++ config/initializers/avo.rb
@@ -2,7 +2,7 @@
 # The values disaplayed here are the default ones. Uncomment and change them to fit your needs.
 Avo.configure do |config|
   ## == Routing ==
-  config.root_path = '/avo'
+  config.root_path = "/avo"
   # used only when you have custom `map` configuration in your config.ru
   # config.prefix_path = "/internal"
 
@@ -18,9 +18,11 @@ Avo.configure do |config|
   end
 
   ## == Authentication ==
-  # config.current_user_method = :current_user
-  # config.authenticate_with do
-  # end
+  config.current_user_method = :current_user
+
+  config.authenticate_with do
+    authenticate_user!
+  end
 
   ## == Authorization ==
   # config.is_admin_method = :is_admin
